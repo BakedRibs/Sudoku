3a79aa89d4ecd9c4e712e0779537499809b2a620
BakedRibs <18101325879@163.com>
2018-03-07 15:39:41 +0800

    20180307-1539

--- __init__.py
+++ __init__.py
@@ -50,17 +50,38 @@ class SudokuGrid(QWidget):
         
     def sudokuGridInit(self):
         self.setGridItem(1, 2, 2)
-        pass
+        self.setGridItem(1, 4, 5)
+        self.setGridItem(0, 6, 1)
+        self.setGridItem(0, 8, 3)
+        self.setGridItem(3, 0, 1)
+        self.setGridItem(3, 5, 7)
+        self.setGridItem(4, 3, 9)
+        self.setGridItem(4, 5, 3)
+        self.setGridItem(4, 7, 5)
+        self.setGridItem(5, 2, 4)
+        self.setGridItem(5, 7, 2)
+        self.setGridItem(6, 0, 7)
+        self.setGridItem(7, 0, 3)
+        self.setGridItem(7, 4, 6)
+        self.setGridItem(8, 4, 4)
+        self.setGridItem(8, 6, 2)
+        self.setGridItem(8, 7, 6)
+        
+        for i in range(9):
+            for j in range(9):
+                numLen = len(self.sudokuNum[i][j])
+                if numLen == 1:
+                    temp = QTableWidgetItem(str(self.sudokuNum[i][j][0]))
+                    temp.setTextAlignment(Qt.AlignCenter)
+                    temp.setFont(QFont("Microsoft YaHei", 18))
+                    self.sudokuGrid[i//3][j//3].setItem(i%3, j%3, temp)
         
     def setGridItem(self, row, column, number):
         i = row // 3
         j = column // 3
         ii = row % 3
         jj = column % 3
-        temp = QTableWidgetItem(str(number))
-        temp.setTextAlignment(Qt.AlignCenter)
-        temp.setFont(QFont("Microsoft YaHei", 18))
-        self.sudokuGrid[i][j].setItem(ii, jj, temp)
+        self.setSudokuGrid(number, i, j, ii, jj)
         self.sudokuNum[row][column] = [number]
         self.removeOthers(row, column, number)
         
@@ -72,7 +93,7 @@ class SudokuGrid(QWidget):
             self.removeNum(o, column, number)
         for p in range(3):
             for q in range(3):
-                self.removeNum(i+p, j+q, number)
+                self.removeNum(i*3+p, j*3+q, number)
             
     def removeNum(self, row, column, number):
         numLen = len(self.sudokuNum[row][column])
@@ -80,6 +101,12 @@ class SudokuGrid(QWidget):
             return
         if number in self.sudokuNum[row][column]:
             self.sudokuNum[row][column].remove(number)
+            
+    def setSudokuGrid(self, number, i, j, ii, jj):
+        temp = QTableWidgetItem(str(number))
+        temp.setTextAlignment(Qt.AlignCenter)
+        temp.setFont(QFont("Microsoft YaHei", 18))
+        self.sudokuGrid[i][j].setItem(ii, jj, temp)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
