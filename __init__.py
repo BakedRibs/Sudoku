ce43ddfa2eb0aa25519ac5326e15aec72675979c
BakedRibs <18101325879@163.com>
2018-03-08 17:15:32 +0800

    20180308-1715

--- __init__.py
+++ __init__.py
@@ -49,37 +49,35 @@ class SudokuGrid(QWidget):
         self.fillBt.clicked.connect(self.fillBtClicked)
         
     def sudokuGridInit(self):
-        self.insertList = []
         for i in range(3):
             for j in range(3):
                 for p in range(3):
                     for q in range(3):
                         self.setSudokuGrid('', i, j, p, q)
                         
-        self.sudokuNum = []
-        for i in range(9):
-            self.sudokuNum.append([])
-            for j in range(9):
-                smallNum = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                self.sudokuNum[i].append(smallNum)
+        self.sudokuNumInit()
                 
-        self.setGridItem(1, 2, 2)
-        self.setGridItem(1, 4, 5)
-        self.setGridItem(0, 6, 1)
-        self.setGridItem(0, 8, 3)
-        self.setGridItem(3, 0, 1)
-        self.setGridItem(3, 5, 7)
-        self.setGridItem(4, 3, 9)
-        self.setGridItem(4, 5, 3)
-        self.setGridItem(4, 7, 5)
-        self.setGridItem(5, 2, 4)
-        self.setGridItem(5, 7, 2)
-        self.setGridItem(6, 0, 7)
-        self.setGridItem(7, 0, 3)
-        self.setGridItem(7, 4, 6)
-        self.setGridItem(8, 4, 4)
-        self.setGridItem(8, 6, 2)
-        self.setGridItem(8, 7, 6)
+        self.insertList = []
+        self.insertList.append([1, 2, 2])
+        self.insertList.append([1, 4, 5])
+        self.insertList.append([0, 6, 1])
+        self.insertList.append([0, 8, 3])
+        self.insertList.append([3, 0, 1])
+        self.insertList.append([3, 5, 7])
+        self.insertList.append([4, 3, 9])
+        self.insertList.append([4, 5, 3])
+        self.insertList.append([4, 7, 5])
+        self.insertList.append([5, 2, 4])
+        self.insertList.append([5, 7, 2])
+        self.insertList.append([6, 0, 7])
+        self.insertList.append([7, 0, 3])
+        self.insertList.append([7, 4, 6])
+        self.insertList.append([8, 4, 4])
+        self.insertList.append([8, 6, 2])
+        self.insertList.append([8, 7, 6])
+        
+        for i in range(len(self.insertList)):
+            self.setGridItem(self.insertList[i][0], self.insertList[i][1], self.insertList[i][2])
         
         self.showAllAlone()
         
@@ -91,6 +89,14 @@ class SudokuGrid(QWidget):
         self.setSudokuGrid(number, i, j, ii, jj)
         self.sudokuNum[row][column] = [number]
         self.removeOthers(row, column, number)
+        
+    def sudokuNumInit(self):
+        self.sudokuNum = []
+        for i in range(9):
+            self.sudokuNum.append([])
+            for j in range(9):
+                smallNum = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                self.sudokuNum[i].append(smallNum)
         
     def removeOthers(self, row, column, number):
         i = row // 3
@@ -132,15 +138,21 @@ class SudokuGrid(QWidget):
                 lenTotal += len(self.sudokuNum[i][j])
         while lenTotal > 81:
             lenMinButOne = 9
+            minI = 0
+            minJ = 0
             for i in range(9):
                 for j in range(9):
                     if len(self.sudokuNum[i][j]) <= lenMinButOne:
                         if len(self.sudokuNum[i][j]) != 1:
                             lenMinButOne = len(self.sudokuNum[i][j])
-            for i in range(9):
-                for j in range(9):
-                    if len(self.sudokuNum[i][j]) == lenMinButOne:
-                        pass
+                            minI = i
+                            minJ = j
+
+            currentInsert = []
+            for cur in range(lenMinButOne):
+                currentInsert.append([minI, minJ, self.sudokuNum[minI][minJ][cur]])
+            self.insertList.append(currentInsert)
+            pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
